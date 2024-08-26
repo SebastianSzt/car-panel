881c8b81854fd6beb713f51aa3e35cd8d5ed1fca
SebastianSzt <sebastiansztankowski@gmail.com>
2024-08-26 11:51:52 +0200

    Add admin controllers and views for brands and models

---
+++ app/controllers/admin/brands_controller.rb
@@ -0,0 +1,79 @@
+class Admin::BrandsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :admin_only
+  before_action :set_brand, only: %i[ show edit update destroy ]
+
+  # GET /brands or /brands.json
+  def index
+    @brands = Brand.page(params[:page]).per(10)
+  end
+
+  # GET /brands/1 or /brands/1.json
+  def show
+  end
+
+  # GET /brands/new
+  def new
+    @brand = Brand.new
+  end
+
+  # GET /brands/1/edit
+  def edit
+  end
+
+  # POST /brands or /brands.json
+  def create
+    @brand = Brand.new(brand_params)
+
+    respond_to do |format|
+      if @brand.save
+        format.html { redirect_to brand_url(@brand), notice: "Brand was successfully created." }
+        format.json { render :show, status: :created, location: @brand }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @brand.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /brands/1 or /brands/1.json
+  def update
+    respond_to do |format|
+      if @brand.update(brand_params)
+        format.html { redirect_to brand_url(@brand), notice: "Brand was successfully updated." }
+        format.json { render :show, status: :ok, location: @brand }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @brand.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /brands/1 or /brands/1.json
+  def destroy
+    @brand = Brand.find(params[:id])
+    @brand.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to brands_url, notice: "Brand was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_brand
+      @brand = Brand.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def brand_params
+      params.require(:brand).permit(:name)
+    end
+
+    def admin_only
+      unless current_user&.admin?
+        redirect_to root_path, alert: "You are not allowed to access this page."
+      end
+    end
+end
