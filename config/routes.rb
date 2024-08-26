6cc7fb5c86264608ebe0d384475179ecbddae6d6
SebastianSzt <sebastiansztankowski@gmail.com>
2024-08-26 11:53:19 +0200

    Update home page, add devise links, fix sign out

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   resources :models
   resources :brands
   devise_for :users
+
+  root "brands#index"
+
+  namespace :admin do
+    resources :brands
+    resources :models
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
