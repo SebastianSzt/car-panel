355a2b1803be40823dec0292eefb99f51bef065a
SebastianSzt <sebastiansztankowski@gmail.com>
2024-08-27 13:41:26 +0200

    Add validation for data in Brand and Model models

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,4 @@
 class Brand < ApplicationRecord
   has_many :models, dependent: :destroy
+  validates :name, presence: true
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,4 +1,5 @@
 class Model < ApplicationRecord
   attr_accessor :brand_name
   belongs_to :brand
+  validates :name, :year, presence: true
 end
